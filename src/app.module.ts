b0f20c4062363f9c596e6ac0f88f184ff187a107
Chhayly Sreng <chhayly.sreng@gmail.com>
2021-11-01 11:50:47 +0700

    create crud for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import config from 'ormconfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
